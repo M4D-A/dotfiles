c401ab299af972f7b4e2b332325ecfc5d3b8d7f6
M4D-A <35253296+M4D-A@users.noreply.github.com>
2024-07-19 14:03:34 +0200

    Update pre_init.sh

--- pre_init.sh
+++ pre_init.sh
@@ -5,8 +5,10 @@ wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo t
 sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
 GH_REPO=$(echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main")
 echo $GH_REPO | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update 
 
-sudo apt update sudo apt install -y gh git
+sudo apt install -y gh git
+
 gh auth login
 gh repo clone dotfiles
 cd dotfiles
