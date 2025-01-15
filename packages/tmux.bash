428e869e8c5edeb4fe0ece0131373dca7f99339e
M4DA <jagielski.adamm@gmail.com>
2025-01-15 15:31:29 +0100

    tmux reconfig

--- packages/tmux.bash
+++ packages/tmux.bash
@@ -1,3 +1,8 @@
 # Tmux - terminal multiplexer
 sudo apt-get install tmux
+mkdir -p ~/.tmux/plugins
+mkdir -p ~/.tmux/plugins/tpm
+mkdir -p ~/.tmux/plugins/catppuccin
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
