66c489274e899c01bf0cf0e98357d2eaf29ec488
M4DA <jagielski.adamm@gmail.com>
2025-01-15 11:26:16 +0100

    Minor edits in init.bash

--- init.bash
+++ init.bash
@@ -21,18 +21,18 @@ declare -a packages=(
   "tmux"
   "zoxide"
 
-  "stow"
+  "stow" # must be kept last to link all created config files
 )
 
 for name in "${packages[@]}"
 do
   echo -n "Installing ${name} packages... "
   bash "packages/${name}.bash" > "${name}.out" 2> "${name}.err"
-  OK=$?
-  if [ ${OK} -eq 0 ] ; then 
+  STATUS=$?
+  if [ ${STATUS} -eq 0 ] ; then 
     rm ${name}.out ${name}.err
     echo "✓"
   else
-    echo "✗ (${OK}), check ${name}[.err|.out] files"
+    echo "✗ (${STATUS}), check ${name}[.err|.out] files"
   fi
 done
