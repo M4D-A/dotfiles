24f7ceb74d9411691e901ad0401ed596dc1daf84
M4DA <jagielski.adamm@gmail.com>
2025-01-27 14:45:03 +0100

    Irrelevant

--- packages/neovim.bash
+++ packages/neovim.bash
@@ -3,6 +3,9 @@ wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/bin/nvim
 
+sudo npm install -g neovim
+sudo npm install -g tree-sitter-cli
+
 # AstroNVIM custom repo
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
@@ -10,5 +13,3 @@ rm -rf ~/.local/state/nvim
 rm -rf ~/.cache/nvim
 git clone https://github.com/M4D-A/AstroNvimConfig.git ~/.config/nvim
 
-sudo npm install -g neovim
-sudo npm install -g tree-sitter-cli
